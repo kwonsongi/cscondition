57c0d30bb3e2205f58710a4e340046c16e86bcae
Kwon Song I <s2018s22@e-mirim.hs.kr>
2020-05-06 21:40:40 +0900

    #4 13-6 삼항연산자 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -21,7 +21,7 @@ namespace cscondition
                 Console.WriteLine("홀수 입니다.");
             }
             */
-            switch(input % 2)
+            /*switch (input % 2)
             {
                 case 0:
                     Console.WriteLine("짝수 입니다.");
@@ -30,6 +30,11 @@ namespace cscondition
                     Console.WriteLine("홀수 입니다.");
                     break;
             }
+            */
+            Console.WriteLine(input % 2 == 0 ? "짝수입니다." : "홀수입니다.");
+            Console.WriteLine(input > 0 ? "자연수입니다." : "자연수가 아닙니다.");
+            
+            //현재 시간 구하기
             Console.WriteLine(DateTime.Now.Year);
             Console.WriteLine(DateTime.Now.Month);
             Console.WriteLine(DateTime.Now.Day);
