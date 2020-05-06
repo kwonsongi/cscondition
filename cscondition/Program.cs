8bb0359260c7b83d537d1a31b5991437e8dc920f
Kwon Song I <s2018s22@e-mirim.hs.kr>
2020-05-06 21:23:58 +0900

    #2 13-4 if else if 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -32,9 +32,7 @@ namespace cscondition
             {
                 Console.WriteLine("아침 먹을 시간입니다.");
             }
-            else
-            {
-                if (DateTime.Now.Hour < 15)
+            else if (DateTime.Now.Hour < 15)
                 {
                     Console.WriteLine("점심 먹을 시간입니다.");
                 }
@@ -45,4 +43,3 @@ namespace cscondition
             }
         }
     }
-}
